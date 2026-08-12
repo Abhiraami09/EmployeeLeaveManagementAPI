24747d5fe380d303d7728f1d4616aceea9335c2a
Abhiraami09 <abhiraami2006@gmail.com>
2026-08-12 10:47:02 +0530

    created data folder and added database context for Entity Framework Core

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EmployeeLeaveManagementAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeLeaveManagementAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+    }
+}
