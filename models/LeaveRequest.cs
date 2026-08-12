986c3844a2c69b3fe7adfd64ae875c112468b525
Abhiraami09 <abhiraami2006@gmail.com>
2026-08-12 10:41:04 +0530

    created model folder and added 3 classes into that

---
+++ models/LeaveRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace EmployeeLeaveManagementAPI.Models
+{
+    public class LeaveRequest
+    {
+        public int Id { get; set; }
+
+        public int EmployeeId { get; set; }
+
+        public int LeaveTypeId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Reason { get; set; } = string.Empty;
+
+        public string Status { get; set; } = "Pending";
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+        public Employee Employee { get; set; } = null!;
+
+        public LeaveType LeaveType { get; set; } = null!;
+    }
+}
---
+++ models/LeaveType.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeLeaveManagementAPI.Models
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int MaximumDays { get; set; }
+    }
+}
