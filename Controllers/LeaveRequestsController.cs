f784d81928f4f5156c257d7b64e4f55e9b7af7d2
Abhiraami09 <abhiraami2006@gmail.com>
2026-08-12 11:06:51 +0530

    Add LeaveTypes Controller with CRUD endpoints

---
+++ Controllers/LeaveRequestsController.cs
@@ -0,0 +1,88 @@
+﻿using EmployeeLeaveManagementAPI.Data;
+using EmployeeLeaveManagementAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeLeaveManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveRequestsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LeaveRequestsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/LeaveRequests
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LeaveRequest>>> GetLeaveRequests()
+        {
+            return await _context.LeaveRequests.ToListAsync();
+        }
+
+        // GET: api/LeaveRequests/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LeaveRequest>> GetLeaveRequest(int id)
+        {
+            var leaveRequest = await _context.LeaveRequests.FindAsync(id);
+
+            if (leaveRequest == null)
+            {
+                return NotFound();
+            }
+
+            return leaveRequest;
+        }
+
+        // POST: api/LeaveRequests
+        [HttpPost]
+        public async Task<ActionResult<LeaveRequest>> CreateLeaveRequest(
+            LeaveRequest leaveRequest)
+        {
+            _context.LeaveRequests.Add(leaveRequest);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetLeaveRequest),
+                new { id = leaveRequest.Id },
+                leaveRequest);
+        }
+
+        // PUT: api/LeaveRequests/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateLeaveRequest(
+            int id,
+            LeaveRequest leaveRequest)
+        {
+            if (id != leaveRequest.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(leaveRequest).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/LeaveRequests/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLeaveRequest(int id)
+        {
+            var leaveRequest = await _context.LeaveRequests.FindAsync(id);
+
+            if (leaveRequest == null)
+            {
+                return NotFound();
+            }
+
+            _context.LeaveRequests.Remove(leaveRequest);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Controllers/LeaveTypesController.cs
@@ -0,0 +1,88 @@
+﻿using EmployeeLeaveManagementAPI.Data;
+using EmployeeLeaveManagementAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeLeaveManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveTypesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LeaveTypesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/LeaveTypes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LeaveType>>> GetLeaveTypes()
+        {
+            return await _context.LeaveTypes.ToListAsync();
+        }
+
+        // GET: api/LeaveTypes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LeaveType>> GetLeaveType(int id)
+        {
+            var leaveType = await _context.LeaveTypes.FindAsync(id);
+
+            if (leaveType == null)
+            {
+                return NotFound();
+            }
+
+            return leaveType;
+        }
+
+        // POST: api/LeaveTypes
+        [HttpPost]
+        public async Task<ActionResult<LeaveType>> CreateLeaveType(
+            LeaveType leaveType)
+        {
+            _context.LeaveTypes.Add(leaveType);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetLeaveType),
+                new { id = leaveType.Id },
+                leaveType);
+        }
+
+        // PUT: api/LeaveTypes/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateLeaveType(
+            int id,
+            LeaveType leaveType)
+        {
+            if (id != leaveType.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(leaveType).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/LeaveTypes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLeaveType(int id)
+        {
+            var leaveType = await _context.LeaveTypes.FindAsync(id);
+
+            if (leaveType == null)
+            {
+                return NotFound();
+            }
+
+            _context.LeaveTypes.Remove(leaveType);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
